cd7c5f54659f4d5752a8a0437ac7b2ddc35d9f9d
Daniele Tolomelli <daniel.tolomelli@gmail.com>
2020-02-10 08:43:57 +0100

    Added a reset command that doesn't work

--- content_script.js
+++ content_script.js
@@ -1,188 +1,253 @@
 (
 
-    function () {
+  function () {
 
-        function extractValue(data) {
-            return data[Object.keys(data)[0]]
-        }
+    const ie = new InputEvent("input");
 
-        async function fillData() {
-            var email = await browser.storage.local.get("email");
-            document.getElementById("username").value = extractValue(email);
-
-            var password = await browser.storage.local.get("password");
-            document.getElementById("password").value = extractValue(password);
-
-            var pin = await browser.storage.local.get("pin");
-            document.getElementById("pin").value = extractValue(pin);
-
-            document.getElementById("login-button").click();
-        }
-
-        async function fillData_ar() {
-            var email = await browser.storage.local.get("email");
-            document.getElementById("nome_utente_ar").value = extractValue(email);
-
-            var password = await browser.storage.local.get("password");
-            document.getElementById("password_ar").value = extractValue(password);
-
-            var pin = await browser.storage.local.get("pin");
-            document.getElementById("codicepin").value = extractValue(pin);
-
-            document.getElementsByClassName("conferma").item(0).click();
-        }
-
-        async function fillWizard() {
-            $.get('http://localhost:3000/api/readjson').done(async (jsonData) => {
-                var data = JSON.parse(jsonData);
-                var discount = data["sconto"];
-
-                await new Promise(r => setTimeout(r, 1000));
-
-                const ie = new InputEvent("input");
-                document.getElementById("i2_4_1").value = data["pagamento in contanti"];
-                document.getElementById("i2_4_1").dispatchEvent(ie);
-                document.getElementById("i2_4_2").value = data["pagamento elettronico"];
-                document.getElementById("i2_4_2").dispatchEvent(ie);
-
-                for (var i = 0; i < data["voci"].length; i++) {
-                    console.log(`i = ${i}`);
-
-                    document.getElementById(`i2_2_1_r${i}`).value = '1,00';
-                    document.getElementById(`i2_2_1_r${i}`).dispatchEvent(ie);
-                    console.log(`${data["voci"][i]["nome"].toLowerCase()} - $${data["voci"][i]["prezzo"]}`)
-                    document.getElementById(`i2_2_3_r${i}`).value = `${data["voci"][i]["prezzo"]}`;
-                    document.getElementById(`i2_2_3_r${i}`).dispatchEvent(ie);
-                    document.getElementById(`i2_2_4_r${i}`).value = discount;
-                    document.getElementById(`i2_2_4_r${i}`).dispatchEvent(ie);
-                }
-
-                await new Promise(r => setTimeout(r, 1000));
-
-                for (var i = 0; i < data["voci"].length; i++) {
-                    console.log(`i = ${i}`);
-
-                    document.getElementsByClassName("btn-info").item(i).click();
-                    var name = data["voci"][i]["nome"].toLowerCase();
-                    await new Promise(r => setTimeout(r, 500));
-                    document.getElementById('filtro').value = name;
-                    document.getElementById('filtro').dispatchEvent(ie);
-                    var scopes = document.getElementsByClassName("ng-scope");
-                    for (let j = 0; j < scopes.length; j++) {
-                        if (scopes.item(j).innerHTML.toLowerCase().substr(3) == data["voci"][i]["nome"].toLowerCase()) {
-                            console.log(`Found ${name}!`);
-                            scopes.item(j).click();
-                            break;
-                        }
-                    }
-                    document.getElementsByClassName("btn-default").item(0).click();
-                    await new Promise(r => setTimeout(r, 500));
-                }
-
-            }, "json");
-        }
-
-        function isLoginURL() {
-            return document.URL.endsWith("login.jsp") || document.URL.endsWith("portale/");
-        }
-
-        function automate() {
-            var URL = document.URL;
-
-            console.log(`Page has URL ${URL}`);
-            if (URL.endsWith("Messaggi.jsp")) {
-                document.getElementById("boxfatturazione").children[0].click();
-            } else if (URL.endsWith("FatturazioneElettronica.jsp")) {
-                document.getElementsByClassName("panel-footer").item(1).children[0].click();
-            } else if (URL.endsWith("informativa")) {
-                document.getElementsByClassName("prosegui").item(0).children[0].click();
-                // } else if (URL.includes("documenticommercialionline")) {
-                //     // Genera il mio documento
-                //     document.getElementsByClassName("mylist").item(0).children[0].children[0].click()
-            } else if (URL.endsWith("login.jsp")) {
-                fillData_ar();
-            } else if (URL.includes("generazione/wizard")) {
-                fillWizard();
-            }
-
-            if (URL.endsWith("portale/")) {
-                if (document.getElementById("username") != undefined) {
-                    fillData();
-                }
-                document.getElementsByClassName("link_spa").item(3).click();
-            }
-
-            if (URL.endsWith("home")) {
-                if (URL.endsWith("portale/web/guest/home")) {
-                    document.getElementsByClassName("link_spa").item(3).click();
-                } else if (document.getElementById("chiudi_informativa") != undefined) {
-                    document.getElementById("chiudi_informativa").click();
-                } else if (URL.includes("documenticommercialionline")) {
-                    new Promise(r => setTimeout(r, 500)).then(() => {
-                        var maybelist = document.getElementsByClassName("mylist").item(0);
-                        if (maybelist !== null) {
-                            console.log("maybe list != null");
-                            maybelist.children[0].children[0].click();
-                        }
-                    });
-                }
-            }
-
-        }
-
-        browser.runtime.onMessage.addListener((message) => {
-            console.log(`Received ${message}`);
-            if (Object.keys(message).includes("action")) {
-                if (message['action'] == 'automate') {
-                    checkForAutomation();
-                }
-                return;
-            }
-            if (document.getElementById("nome_utente_ar") !== null) {
-                fillData_ar();
-            }
-
-            if (document.getElementById("username") !== null) {
-                fillData();
-            }
-        });
-
-        browser.storage.local.get(("go-form"), (goform) => {
-            console.log(`go-form: ${extractValue(goform)}`);
-
-        })
-
-        function checkForAutomation() {
-            browser.storage.local.get("activated")
-                .then((value) => {
-                    var activated = extractValue(value);
-                    if (activated) {
-                        return automate();
-                    }
-
-                    browser.storage.local.get(("go-form"), (go_form) => {
-                        if (!extractValue(go_form))
-                            return;
-                        if (document.URL.includes("generazione/wizard")) {
-                            browser.storage.local.set({ "go-form": false });
-                        } else {
-                            console.log("Automating anyway");
-                            automate();
-                        }
-                    });
-
-                });
-        }
-
-        checkForAutomation();
-
-
-
-        // browser.tabs.query({ active: true, currentWindow: true })
-        //     .then((tab) => {
-        //         console.log(`Loaded page with URL ${tab.url}`);
-        //     })
+    function extractValue(data) {
+      return data[Object.keys(data)[0]]
     }
+
+    function clearForm() {
+      console.log("Clearing form");
+      
+      document.getElementById("i2_4_1").value = "";
+      document.getElementById("i2_4_1").dispatchEvent(ie);
+      document.getElementById("i2_4_2").value = "";
+      document.getElementById("i2_4_2").dispatchEvent(ie);
+
+      console.log("hello");
+      for (var i = 0; i < 9; i++) {
+        document.getElementById(`i2_2_1_r${i}`).value = '';
+        document.getElementById(`i2_2_1_r${i}`).dispatchEvent(ie);
+        document.getElementById(`i2_2_2_r${i}`).value = '';
+        document.getElementById(`i2_2_2_r${i}`).dispatchEvent(ie);
+        document.getElementById(`i2_2_3_r${i}`).value = '';
+        document.getElementById(`i2_2_3_r${i}`).dispatchEvent(ie);
+        document.getElementById(`i2_2_4_r${i}`).value = '';
+        document.getElementById(`i2_2_4_r${i}`).dispatchEvent(ie);
+
+      }
+    }
+
+    async function fillData() {
+      var email = await browser.storage.local.get("email");
+      document.getElementById("username").value = extractValue(email);
+
+      var password = await browser.storage.local.get("password");
+      document.getElementById("password").value = extractValue(password);
+
+      var pin = await browser.storage.local.get("pin");
+      document.getElementById("pin").value = extractValue(pin);
+
+      document.getElementById("login-button").click();
+    }
+
+    async function fillData_ar() {
+      var email = await browser.storage.local.get("email");
+      document.getElementById("nome_utente_ar").value = extractValue(email);
+
+      var password = await browser.storage.local.get("password");
+      document.getElementById("password_ar").value = extractValue(password);
+
+      var pin = await browser.storage.local.get("pin");
+      document.getElementById("codicepin").value = extractValue(pin);
+
+      document.getElementsByClassName("conferma").item(0).click();
+    }
+
+    async function fillWizard() {
+      $.get('http://localhost:3000/api/readjson').done(async (jsonData) => {
+        var data = JSON.parse(jsonData);
+        console.log(data);
+
+        var discount = data["sconto"];
+
+        await new Promise(r => setTimeout(r, 1000));
+
+        document.getElementById("i2_4_1").value = data["pagamento in contanti"];
+        document.getElementById("i2_4_1").dispatchEvent(ie);
+        document.getElementById("i2_4_2").value = data["pagamento elettronico"];
+        document.getElementById("i2_4_2").dispatchEvent(ie);
+
+        for (var i = 0; i < data["voci"].length; i++) {
+          console.log(`i = ${i}`);
+
+          document.getElementById(`i2_2_1_r${i}`).value = '1,00';
+          document.getElementById(`i2_2_1_r${i}`).dispatchEvent(ie);
+          console.log(`${data["voci"][i]["nome"].toLowerCase()} - $${data["voci"][i]["prezzo"]}`)
+          document.getElementById(`i2_2_3_r${i}`).value = `${data["voci"][i]["prezzo"]}`;
+          document.getElementById(`i2_2_3_r${i}`).dispatchEvent(ie);
+          document.getElementById(`i2_2_4_r${i}`).value = discount;
+          document.getElementById(`i2_2_4_r${i}`).dispatchEvent(ie);
+        }
+
+        await new Promise(r => setTimeout(r, 1000));
+
+        // btn-info in [4, 11]
+
+        for (var i = 0; i < data["voci"].length; i++) {
+          console.log(`i = ${i}, typeof(i) = ${typeof (i)}`);
+
+          console.log(document.getElementsByTagName("button").length);
+          var filterButton = document.getElementsByTagName("button")[3 + i];
+          console.log(filterButton == null);
+          filterButton.click();
+          // document.getElementsByClassName("btn-info").item(i).click();
+          var name = data["voci"][i]["nome"].toLowerCase();
+          await new Promise(r => setTimeout(r, 1500));
+          document.getElementById('filtro').value = name;
+          document.getElementById('filtro').dispatchEvent(ie);
+          var scopes = document.getElementsByClassName("ng-scope");
+          for (let j = 0; j < scopes.length; j++) {
+            if (scopes.item(j).innerHTML.toLowerCase().substr(3) == data["voci"][i]["nome"].toLowerCase()) {
+              console.log(`Found ${name}!`);
+              scopes.item(j).click();
+              break;
+            }
+          }
+          document.getElementsByClassName("btn-default").item(0).click();
+          await new Promise(r => setTimeout(r, 500));
+        }
+
+        for (var i = 0; i < data["voci"].length; i++) {
+          document.getElementById(`i2_2_3_r${i}`).value = `${data["voci"][i]["prezzo"]}`;
+          document.getElementById(`i2_2_3_r${i}`).dispatchEvent(ie);
+        }
+
+      }, "json");
+    }
+
+    function isLoginURL() {
+      return document.URL.endsWith("login.jsp") || document.URL.endsWith("portale/");
+    }
+
+    function automate() {
+      var URL = document.URL;
+
+      console.log(`Page has URL ${URL}`);
+      if (URL.endsWith("Messaggi.jsp")) {
+        document.getElementById("boxfatturazione").children[0].click();
+      } else if (URL.endsWith("FatturazioneElettronica.jsp")) {
+        document.getElementsByClassName("panel-footer").item(1).children[0].click();
+      } else if (URL.endsWith("informativa")) {
+        document.getElementsByClassName("prosegui").item(0).children[0].click();
+        // } else if (URL.includes("documenticommercialionline")) {
+        //     // Genera il mio documento
+        //     document.getElementsByClassName("mylist").item(0).children[0].children[0].click()
+      } else if (URL.endsWith("login.jsp")) {
+        fillData_ar();
+        // } else if (URL.includes("generazione/wizard")) {
+        //   fillWizard();
+      }
+
+      if (URL.endsWith("portale/")) {
+        if (document.getElementById("username") != undefined) {
+          fillData();
+        }
+        document.getElementsByClassName("link_spa").item(3).click();
+      }
+
+      if (URL.endsWith("home")) {
+        if (URL.endsWith("portale/web/guest/home")) {
+          document.getElementsByClassName("link_spa").item(3).click();
+        } else if (document.getElementById("chiudi_informativa") != undefined) {
+          document.getElementById("chiudi_informativa").click();
+        } else if (URL.includes("documenticommercialionline")) {
+          new Promise(r => setTimeout(r, 500)).then(() => {
+            var maybelist = document.getElementsByClassName("mylist").item(0);
+            if (maybelist !== null) {
+              console.log("maybe list != null");
+              maybelist.children[0].children[0].click();
+              browser.storage.local.set({ "go-form": false });
+            }
+          });
+        }
+      }
+
+    }
+
+    browser.runtime.onMessage.addListener((message) => {
+      console.log(`Received ${message}`);
+      if (Object.keys(message).includes("action")) {
+        if (message['action'] == 'automate') {
+          return checkForAutomation();
+        }
+
+        if (message['action'] == 'fill-form') {
+          console.log("Filling wizard");
+          return fillWizard();
+        }
+
+        if (message['action'] == 'clear-form') {
+          return clearForm();
+        }
+
+        console.log("Error: action not 'automate' nor 'fill-form' nor 'clear-form'");
+
+      }
+
+      if (document.getElementById("nome_utente_ar") !== null) {
+        fillData_ar();
+      }
+
+      if (document.getElementById("username") !== null) {
+        fillData();
+      }
+    });
+
+    browser.storage.local.get(("go-form"), (goform) => {
+      console.log(`go-form: ${extractValue(goform)}`);
+
+    })
+
+    function checkForAutomation() {
+      browser.storage.local.get(("go-form"), (go_form) => {
+        if (!extractValue(go_form))
+          return;
+        if (document.URL.includes("generazione/wizard")) {
+          console.log("Disabling go-form");
+          browser.storage.local.set({ "go-form": false });
+        } else {
+          console.log("Automating anyway");
+          automate();
+        }
+      });
+    }
+
+    function checkForLogin() {
+      browser.storage.local.get(("login"), (loginUnextracted) => {
+        var login = extractValue(loginUnextracted);
+        if (!login)
+          return;
+
+        console.log(`Logging in ${login}`);
+
+        if (document.getElementById("nome_utente_ar") !== null) {
+          browser.storage.local.set({ "login": false }, () => {
+            fillData_ar();
+          })
+        }
+
+        if (document.getElementById("username") !== null) {
+          browser.storage.local.set({ "login": false }, () => {
+            fillData();
+          });
+        }
+      });
+    }
+
+    checkForLogin();
+
+    checkForAutomation();
+
+
+
+    // browser.tabs.query({ active: true, currentWindow: true })
+    //     .then((tab) => {
+    //         console.log(`Loaded page with URL ${tab.url}`);
+    //     })
+  }
 
 
 )();
--- popup.js
+++ popup.js
@@ -1,105 +1,177 @@
 var data = {};
 
 function extractValue(data) {
-    return data[Object.keys(data)[0]]
+  return data[Object.keys(data)[0]]
 }
 
 function sendMessage(message) {
-    browser.tabs.query({
-        currentWindow: true,
-        active: true
-    }).then((tabs) => {
-        console.log(data);
-        browser.tabs.sendMessage(tabs[0].id, message);
-    });
+  browser.tabs.query({
+    currentWindow: true,
+    active: true
+  }).then((tabs) => {
+    console.log(data);
+    browser.tabs.sendMessage(tabs[0].id, message);
+  });
 }
 
 function listenForClicks() {
 
 
-    document.addEventListener("input", (e) => {
-        console.log("received input event");
-        if (e.target.id == "email") {
-            browser.storage.local.set({ "email": e.target.value });
-        }
+  document.addEventListener("input", (e) => {
+    console.log("received input event");
+    if (e.target.id == "email") {
+      browser.storage.local.set({ "email": e.target.value });
+    }
 
-        if (e.target.id == "password") {
-            browser.storage.local.set({ "password": e.target.value });
-        }
+    if (e.target.id == "password") {
+      browser.storage.local.set({ "password": e.target.value });
+    }
 
-        if (e.target.id == "pin") {
-            browser.storage.local.set({ "pin": e.target.value });
-        }
+    if (e.target.id == "pin") {
+      browser.storage.local.set({ "pin": e.target.value });
+    }
 
-        if (e.target.id == "activated") {
-            console.log(`Set activated to ${e.target.checked}`);
-            browser.storage.local.set({ "activated": e.target.checked });
-        }
-
-    });
+  });
 }
 
 ["email", "password", "pin"].forEach((dom, _) => {
-    browser.storage.local.get(dom)
-        .then((element) => {
-            var value = element[Object.keys(element)[0]];
-            if (value === undefined)
-                return;
-            data[`${dom}`] = value;
-            document.getElementById(dom).value = value;
-        });
-});
-
-browser.storage.local.get(("activated"))
-    .then((isActive) => {
-        var value = extractValue(isActive);
-        data['activated'] = value;
-        document.getElementById("activated").checked = value;
-
-    });
-
-
-document.getElementById('btn-go').addEventListener('click', () => {
-    browser.tabs.create({
-        active: true,
-        url: "https://telematici.agenziaentrate.gov.it/Main/login.jsp"
+  browser.storage.local.get(dom)
+    .then((element) => {
+      var value = element[Object.keys(element)[0]];
+      if (value === undefined)
+        return;
+      data[`${dom}`] = value;
+      document.getElementById(dom).value = value;
     });
 });
+
 
 document.getElementById('btn-go-form').addEventListener('click', () => {
-    browser.storage.local.set({'go-form': true}, () => {
-        console.log("Set go-form to true");
-        
-        browser.tabs.query({
-            active: true,
-            currentWindow: true
-        }, (tabs) => {
-            browser.tabs.sendMessage(tabs[0].id, {action: "automate"});
-        });
-        // browser.tabs.query({
-        //     active: true,
-        //     url: "https://telematici.agenziaentrate.gov.it/Main/login.jsp"
-        // });
-    });
-})
+  browser.storage.local.set({ 'go-form': true }, () => {
+    console.log("Set go-form to true");
 
-document.getElementById('btn-fill').addEventListener('click', () => {
-    console.log(document.URL);
-    sendMessage({
-        email: document.getElementById('email').value,
-        password: document.getElementById('password').value,
-        pin: document.getElementById('pin').value,
-    })
+    browser.tabs.query({
+      active: true,
+      currentWindow: true
+    }, (tabs => {
+      console.log(tabs[0].url);
+
+      if (tabs[0].url.includes("agenziaentrate.gov.it")) {
+        browser.tabs.sendMessage(tabs[0].id, { action: "automate" });
+      } else {
+        browser.tabs.create({
+          active: true,
+          url: "https://telematici.agenziaentrate.gov.it/Main/login.jsp"
+        });
+      }
+    }));
+
+
+    // browser.tabs.query({
+    //   active: true,
+    //   currentWindow: true
+    // }, (tabs) => {
+    //   browser.tabs.sendMessage(tabs[0].id, { action: "automate" });
+    // });
+    // browser.tabs.query({
+    //     active: true,
+    //     url: "https://telematici.agenziaentrate.gov.it/Main/login.jsp"
+    // });
+  });
+});
+
+document.getElementById('btn-fill-form').addEventListener('click', () => {
+  browser.tabs.query({
+    active: true,
+    currentWindow: true
+  }, (tabs) => {
+    if (tabs[0].url.includes("generazione/wizard")) {
+      sendMessage({
+        "action": "fill-form"
+      });
+    } else {
+      // TODO
+      // Go to form and then fill
+    }
+  });
+});
+
+document.getElementById('btn-clear-form').addEventListener('click', () => {
+  sendMessage({
+    "action": "clear-form"
+  });
+});
+
+
+// document.getElementById('btn-go').addEventListener('click', () => {
+//   browser.tabs.create({
+//     active: true,
+//     url: "https://telematici.agenziaentrate.gov.it/Main/login.jsp"
+//   });
+// });
+
+// document.getElementById('btn-fill-login').addEventListener('click', () => {
+//   console.log(document.URL);
+//   sendMessage({
+//     email: document.getElementById('email').value,
+//     password: document.getElementById('password').value,
+//     pin: document.getElementById('pin').value,
+//   })
+// });
+
+function _fillLogin() {
+  sendMessage({
+    email: document.getElementById('email').value,
+    password: document.getElementById('password').value,
+    pin: document.getElementById('pin').value,
+  });
+}
+
+document.getElementById('btn-login').addEventListener('click', () => {
+  console.log("Accedi");
+  
+  browser.tabs.query({
+    active: true,
+    currentWindow: true
+  }, (tabs) => {
+    if (tabs[0].url.includes("agenziaentrate.gov") && tabs[0].url.includes("login")) {
+      _fillLogin();
+    } else {
+      browser.storage.local.set({"login": true}, () => {
+        browser.tabs.create({
+          active: true,
+          url: "https://telematici.agenziaentrate.gov.it/Main/login.jsp"
+        });
+      })
+    }
+  });
 });
 
 browser.tabs.executeScript({ file: "/content_script.js" })
-    .then(listenForClicks);
+  .then(listenForClicks);
 
 
 
 fetch("http://localhost:3000/api/readjson")
-    .then((response) => {
-        return response.json();
-    }).then((myJson) => {
-        console.log(myJson);
+  .then((response) => {
+    return response.json();
+  }).then((myJson) => {
+    console.log(myJson);
+  });
+
+fetch("http://localhost:3000/api/credentials")
+  .then((response) => {
+    if (!response.ok)
+      return;
+    response.json().then((creds) => {
+
+      console.log(JSON.stringify(creds));
+
+      browser.storage.local.set({ "email": creds.taxcode });
+      document.getElementById('email').value = creds.taxcode;
+      browser.storage.local.set({ "password": creds.password });
+      document.getElementById('password').value = creds.password;
+      browser.storage.local.set({ "pin": creds.pin });
+      document.getElementById('pin').value = creds.pin;
     });
+  });
